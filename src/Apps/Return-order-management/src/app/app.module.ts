b8a936abb36f8480648c41a590fe50b049e497b6
Its-ashish <tashish531@gmail.com>
2022-05-15 16:47:17 +0530

    Initaited project

---
+++ src/Apps/Return-order-management/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NbThemeModule } from '@nebular/theme';
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NbThemeModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
