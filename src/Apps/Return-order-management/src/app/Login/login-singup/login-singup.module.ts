20c0785ea37570f3cab9a9fc45e8c04f2eb0b0c4
Its-ashish <tashish531@gmail.com>
2022-05-15 21:27:37 +0530

    Nebular worked

---
+++ src/Apps/Return-order-management/src/app/Login/login-singup/login-singup.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoginSingupRoutingModule } from './login-singup-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LoginSingupRoutingModule
+  ]
+})
+export class LoginSingupModule { }
--- src/Apps/Return-order-management/src/app/app.module.ts
+++ src/Apps/Return-order-management/src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NbThemeModule } from '@nebular/theme';
+import { NbLayoutModule, NbThemeModule } from '@nebular/theme';
+import { HomeComponent } from './Home/home/home.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    NbThemeModule.forRoot()
+    NbThemeModule.forRoot({ name: 'cosmic' }),
+    NbLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
