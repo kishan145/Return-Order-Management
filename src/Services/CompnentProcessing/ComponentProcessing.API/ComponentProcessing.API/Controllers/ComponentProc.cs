ff4401f416b20b40ec9c1110cac003f051124175
Kishan Yadav <kishanyadav145@gmail.com>
2022-05-15 15:07:17 +0530

    [Component Prociessing Impelemented]

---
+++ src/Services/CompnentProcessing/ComponentProcessing.API/ComponentProcessing.API/Controllers/ComponentProc.cs
@@ -0,0 +1,95 @@
+﻿using AutoMapper;
+using ComponentProcessing.API.Models;
+using ComponentProcessing.API.Models.Dtos;
+using ComponentProcessing.API.Repository.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComponentProcessing.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComponentProc : Controller
+    {
+        private IComponentProcessingRepository _cpRepo;
+        private readonly IMapper _mapper;
+
+        public ComponentProc(IComponentProcessingRepository cpRepo, IMapper mapper)
+        {
+            _cpRepo = cpRepo;
+            _mapper = mapper;
+
+        }
+
+        [HttpGet]
+        public IActionResult GetComponentProcessings()
+        {
+            var objList = _cpRepo.GetComponentProcessings();
+
+            var objDto = new List<ComponentProcessingModelDto>();
+
+            foreach (var obj in objList)
+            {
+                objDto.Add(_mapper.Map<ComponentProcessingModelDto>(obj));
+            }
+            return Ok(objDto);
+        }
+
+        [HttpGet("{cpId:int}",Name = "GetComponentProcessing")]
+        public IActionResult GetComponentProcessing(int cpId)
+        {
+            var obj = _cpRepo.GetComponentProcessing(cpId);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            var objDto = _mapper.Map<ComponentProcessingModelDto>(obj);
+            return Ok(objDto);
+        }
+
+        [HttpPost]
+        public IActionResult CreateComponentProcessing([FromBody] CreateDto compDto)
+        {
+            if(compDto == null)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var componentProcessingObj = _mapper.Map<ComponentProcessingModel>(compDto);
+            if(!_cpRepo.CreateComponentProcessing(componentProcessingObj))
+            {
+                ModelState.AddModelError("",$"Something went wrong when saving the record {componentProcessingObj.name}");
+                return StatusCode(500, ModelState);
+            }
+            return CreatedAtRoute("GetComponentProcessing", new { cpId = componentProcessingObj.requestId }, componentProcessingObj);
+        }
+
+        [HttpPatch("{cpId:int}", Name = "UpdateComponentProcessing")]
+        public IActionResult UpdateComponentProcessing(int cpID,[FromBody] UpdateDto compDto)
+        {
+            if (compDto == null || cpID != compDto.requestId)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var componentProcessingObj = _mapper.Map<ComponentProcessingModel>(compDto);
+            if (!_cpRepo.UpdateComponentProcessing(componentProcessingObj))
+            {
+                ModelState.AddModelError("", $"Something went wrong when updating the record {componentProcessingObj.name}");
+                return StatusCode(500, ModelState);
+            }
+            return NoContent();
+
+        }
+
+    }
+}
---
+++ src/Services/CompnentProcessing/ComponentProcessing.API/ComponentProcessing.API/Repository/ComponentProcessingRepository.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ComponentProcessing.API.Data;
+using ComponentProcessing.API.Models;
+using ComponentProcessing.API.Repository.IRepository;
+
+namespace ComponentProcessing.API.Repository
+{
+    public class ComponentProcessingRepository : IComponentProcessingRepository
+
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ComponentProcessingRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+ 
+        public bool ComponentProcessingExists(string name)
+        {
+            bool value = _db.componentProcessings.Any(a => a.name.ToLower().Trim() == name.ToLower().Trim());
+            return value;
+        }
+
+        public bool ComponentProcessingExists(int id)
+        {
+            return _db.componentProcessings.Any(a => a.requestId == id);
+        }
+
+        public bool CreateComponentProcessing(ComponentProcessingModel componentProcessing)
+        {
+            _db.componentProcessings.Add(componentProcessing);
+            return Save();
+        }
+
+        public bool DeleteComponentProcessing(ComponentProcessingModel componentProcessing)
+        {
+            _db.componentProcessings.Remove(componentProcessing);
+            return Save();
+        }
+
+        public ComponentProcessingModel GetComponentProcessing(int ComponentProcessingID)
+        {
+            return _db.componentProcessings.FirstOrDefault(e => e.requestId == ComponentProcessingID);
+        }
+
+        public ICollection<ComponentProcessingModel> GetComponentProcessings()
+        {
+            return _db.componentProcessings.OrderBy(a => a.name).ToList();
+        }
+
+        public bool Save()
+        {
+            return _db.SaveChanges() >= 0 ? true : false;
+        }
+
+        public bool UpdateComponentProcessing(ComponentProcessingModel componentProcessing)
+        {
+            _db.componentProcessings.Update(componentProcessing);
+            return Save();
+        }
+    }
+}
