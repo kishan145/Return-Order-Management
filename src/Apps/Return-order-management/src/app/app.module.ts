5139746f39080781794be494d27f5ee7bf6d91b6
Its-ashish <tashish531@gmail.com>
2022-05-16 21:06:31 +0530

    removed nebular

--- src/Apps/Return-order-management/src/app/app.module.ts
+++ src/Apps/Return-order-management/src/app/app.module.ts
@@ -3,9 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NbButtonModule, NbLayoutModule, NbTabsetModule, NbThemeModule } from '@nebular/theme';
 import { HomeComponent } from './Home/home/home.component';
-import { NbEvaIconsModule } from '@nebular/eva-icons';
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,11 +13,6 @@ import { NbEvaIconsModule } from '@nebular/eva-icons';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    NbThemeModule.forRoot({ name: 'cosmic' }),
-    NbLayoutModule,
-    NbTabsetModule,
-    NbEvaIconsModule,
-    NbButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
