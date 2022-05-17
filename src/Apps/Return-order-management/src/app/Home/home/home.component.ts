eb194341020173c01b2918ea2fc454f1cda3b3f0
Its-ashish <tashish531@gmail.com>
2022-05-17 23:29:22 +0530

    Stepper added, footer incorrect

--- src/Apps/Return-order-management/src/app/Home/home/home.component.ts
+++ src/Apps/Return-order-management/src/app/Home/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Stepper from 'bs-stepper';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  name = 'Project'
+  private stepper!: Stepper;
+  constructor(
+    // private stepper: Stepper
+    ) { }
 
   ngOnInit(): void {
+    this.stepper = new Stepper(document.querySelector('#stepper1')!, {
+      linear: false,
+      animation: true
+    })
   }
 
+  next() {
+    this.stepper.next();
+  }
 }
