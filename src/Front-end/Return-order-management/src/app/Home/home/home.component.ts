74dbbeba1feaeabb743e46bc61b8417620ec73d5
Its-ashish <tashish531@gmail.com>
2022-05-18 18:15:39 +0530

    home screen incomplete

--- src/Front-end/Return-order-management/src/app/Home/home/home.component.ts
+++ src/Front-end/Return-order-management/src/app/Home/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import Stepper from 'bs-stepper';
 
@@ -9,17 +10,40 @@ import Stepper from 'bs-stepper';
 })
 export class HomeComponent implements OnInit {
   public user = false;
+  public step1 = false;
+  public step1Form: FormGroup = new FormGroup({});
+  public step2Form: FormGroup = new FormGroup({});
   name = 'Project'
   constructor(
-    private route: Router
-    // private stepper: Stepper
+    private route: Router,
+    private fb: FormBuilder
     ) { }
 
   ngOnInit(): void {
     
     this.user = history.state.isLoggedIn
+    if (this.user){
+      this.step1 = true;
+      this.step1Form = this.fb.group({
+        name: ['', Validators.required],
+        number: ['', Validators.required],
+        Componentname: ['', Validators.required],
+        quantity: ['', Validators.required],
+        description: ['', Validators.required],
+
+    })
+    }
+  }
+  proceedToNext(){
+    this.step1 = false;
+
+  }
+  save(){
 
   }
 
+  close(){
+    
+  }
   
 }
